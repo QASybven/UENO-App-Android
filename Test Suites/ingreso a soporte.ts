d62ba7550b60c94dd381920e6a684d9b35fb75d2
Jose_Arrieta <Jose_Arrieta@Jose-Arrieta>
2022-04-11 11:02:54 -0400

    prueba2

--- Test Suites/ingreso a soporte.ts
+++ Test Suites/ingreso a soporte.ts
@@ -17,4 +17,106 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/2HU-Soporte/16 Ingresa a la Pantalla Soporte desde la opción Soporte del menú principal de la App</testCaseId>
    </testCaseLink>
+   <testCaseLink>
+      <guid>7f0762b7-cc42-477d-82a5-ffebaac24a2a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/01 Encabezado de la pantalla con banner principal</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3fb7aca6-9c63-4283-a55d-8a07a61dcfba</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6d1f4ff8-bb14-4975-ae10-145b8fc52279</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/02 Pantalla de bienvenida (Hola) sin inicio de sesion</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f1781fc2-04b2-4286-a138-7f1c91933aca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/03 Número de celular (Campo de texto)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cec09a57-2c59-45fd-ae2f-86d729142730</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/04 Pantalla de bienvenida (Hola) con inicio de sesion</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>72bc9bb5-790a-4037-8f01-3f9768ad69d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/05 Contraseña (Campo de texto)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>46765690-8bb4-47fe-966c-085d3520398b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/06 Icono de mostrar contraseña</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3195f001-bce6-4d42-8f77-cf06fc9d327a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/07 Olvidaste tu contraseña (Botón plano)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>524cccf7-d101-4348-b858-0aa7df6e22d0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/08 Ingresar (Botón)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>246898f8-af02-439f-98b3-eecdbb09759b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/09 Menú inferior Soporte (Botón plano)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>69449cc9-84fa-475a-82de-e06a9db5cdc7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/10 Switch Recuérdame</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a809bcf5-2ab3-4277-8a1c-62dcf6cf2c2b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/11 Falla de conexión</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>520780a2-a40a-47e4-8bd0-f4a3ca6315a6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/12 Pantalla Splash</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7ea988da-295e-4253-8179-72273438a149</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/13 Pantalla de Cierre de sesión_Presiona botón</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>734c6cb1-7037-4084-8465-8d2717132419</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/14 Pantalla de Cierre de sesión_Periodo de Inactividad</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ef87c95c-1a53-434a-8f4a-f20e90769edd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/15 Pantalla de Cierre de sesión_Autenticado y en la aplicación</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1fa40435-333e-4be3-befe-375403628cac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1HU-Login-Cierre de Sesión/login</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
