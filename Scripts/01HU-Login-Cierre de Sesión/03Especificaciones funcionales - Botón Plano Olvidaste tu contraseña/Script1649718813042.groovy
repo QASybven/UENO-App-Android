0a41257bc5ba59861c456d9c5650f3459ac4b4af
Jose_Arrieta <Jose_Arrieta@Jose-Arrieta>
2022-04-11 23:34:41 -0400

    2

--- Scripts/01HU-Login-Cierre de Sesión/03Especificaciones funcionales - Botón Plano Olvidaste tu contraseña/Script1649718813042.groovy
+++ Scripts/01HU-Login-Cierre de Sesión/03Especificaciones funcionales - Botón Plano Olvidaste tu contraseña/Script1649718813042.groovy
@@ -17,3 +17,10 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+Mobile.startApplication('C:\\Users\\Jose_Arrieta\\Desktop\\JA\\Proyectos\\Automatizacion de pruebas\\APK para inicio\\crtopyinfinitymobilebanking-release (8).apk', 
+    false)
+
+Mobile.delay(10, FailureHandling.CONTINUE_ON_FAILURE)
+
+Mobile.tap(findTestObject('Login/android.widget.Button - Olvidaste tu contrasea'), 0)
+
