852cb41f9477a61fbdb09baaeb957cb2e4ac708c
Jose_Arrieta <Jose_Arrieta@Jose-Arrieta>
2022-04-21 12:52:15 -0400

    ACT 2104

--- Test Suites/07 HU-Olvidé mi contraseña.ts
+++ Test Suites/07 HU-Olvidé mi contraseña.ts
@@ -5,10 +5,52 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>d2ce94d7-e50f-413d-85bc-8f926755d13e</testSuiteGuid>
+   <testCaseLink>
+      <guid>6805a8ec-5d21-4696-a2d2-501b0ab626ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/85 Pantalla Olvidaste tu contraseña - Estructura de pantalla</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>333f5790-3cfd-425d-bf00-02612bee5f95</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/86 Validaciones en los campos de la pantalla Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7190c813-eaa6-45db-b084-87f35339fdfb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/87 Comportamiento esperado - Pantalla Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>acad2dc5-bb25-4cb7-8446-39181639a87e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/88 Controles y especificaciones de los campos - Pantalla Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bf09e278-44ee-48a8-a164-bc76f25f8960</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/89 Falla de Conexión en la Pantalla Datos Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c3fd9e4e-47b8-4803-8154-8ef7fda36709</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/90 Validación de envío de peticiones Pantalla Datos - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
