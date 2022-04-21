58093ae9b1847a4fc7a07d58a8a67fcaa9b23858
Jose_Arrieta <Jose_Arrieta@Jose-Arrieta>
2022-04-21 14:20:48 -0400

    act

--- Test Suites/07 HU-Olvidé mi contraseña.ts
+++ Test Suites/07 HU-Olvidé mi contraseña.ts
@@ -53,4 +53,116 @@
       <testCaseId>Test Cases/07HU-Olvidé mi contraseña/90 Validación de envío de peticiones Pantalla Datos - Olvidaste tu contraseña</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
+   <testCaseLink>
+      <guid>676276a4-44a7-4bfb-b74b-0eda16d2249c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/100. Comportamiento esperado - Pantalla Nueva Contraseña - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>762e8dc4-51f7-47cc-b79d-12546d6aaff1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/85 Pantalla Olvidaste tu contraseña - Estructura de pantalla</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2ae5d5a9-9aca-4f25-8ff3-0b4051b77ef4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/86 Validaciones en los campos de la pantalla Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>75ae6352-12ef-4e4b-a0ec-90a581f6dc09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/87 Comportamiento esperado - Pantalla Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>420c1ad3-d66b-4be7-bcbd-8cf50df95a47</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/88 Controles y especificaciones de los campos - Pantalla Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>34791551-83e6-4daa-9847-d82280adc801</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/89 Falla de Conexión en la Pantalla Datos Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4b982c25-ab78-4da2-b8b2-6d812e0a9ce3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/90 Validación de envío de peticiones Pantalla Datos - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>320577be-cb6a-4a50-b73e-38d7d71a095e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/91 Estructura de Pantalla - Pantalla Código de seguridad - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>045a1400-7ac5-4a27-b67f-ba05636b8b79</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/92 Solicitar Código de Seguridad - Pantalla Código de seguridad - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>760bc7b0-756b-4722-a8f5-f832beac3fe4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/93 Estructura de la pantalla - Pantalla Nueva Contrseña - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a6ffd786-fe3f-49bb-8c12-5bcfef88eb7c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/94 Campo Nueva Contraseña - Pantalla Nueva Contrseña - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f3febc91-296b-4200-8a3a-8887e60fcce9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/95 Campo Confirmación nueva contraseña - Pantalla Nueva Contraseña - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>497eb9e5-f774-4a72-ab50-56b294a42211</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/96 Botón Resetear - Pantalla Nueva Contraseña - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7c8d6427-1a98-48a7-a5c0-a7f3598ac0c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/97 Validaciones de Front Nueva Contraseña - Pantalla Nueva Contraseña - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4023278e-5c99-4835-aaa5-b6320bc246f0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/98 Falla de conexión - Pantalla Nueva Contraseña - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5d3ad530-f4f3-431d-9355-d847e784cd5a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/07HU-Olvidé mi contraseña/99 Back nativo - Pantalla Nueva Contraseña - Olvidaste tu contraseña</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
