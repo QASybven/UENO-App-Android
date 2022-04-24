7309e1c6692cd30a606702c8cbfa52b06e5aacff
Jose_Arrieta <Jose_Arrieta@Jose-Arrieta>
2022-04-24 16:14:24 -0400

    act

--- Scripts/01HU-Login-Cierre de Sesión/01Especificaciones Funcionales - Primer Login exitoso del Cliente/Script1649710845970.groovy
+++ Scripts/01HU-Login-Cierre de Sesión/01Especificaciones Funcionales - Primer Login exitoso del Cliente/Script1649710845970.groovy
@@ -22,13 +22,11 @@ Mobile.startApplication('C:\\Users\\Jose_Arrieta\\Desktop\\JA\\Proyectos\\Automa
 
 Mobile.delay(10, FailureHandling.STOP_ON_FAILURE)
 
-Mobile.sendKeys(findTestObject('Login/android.widget.EditText - Numero de celular login'), '0982159800', FailureHandling.STOP_ON_FAILURE)
+Mobile.sendKeys(findTestObject('Login/android.widget.EditText - Numero de celular login'), '0982159800', FailureHandling.CONTINUE_ON_FAILURE)
 
 Mobile.setText(findTestObject('Login/android.widget.EditText - tipear contrasea'), 'Kony!12345', 0)
 
 Mobile.tap(findTestObject('Login/android.widget.Button - Ingresar'), 0)
 
-Mobile.checkElement(findTestObject(null), 0)
-
-Mobile.getAttribute(findTestObject(null), '', 0)
+Mobile.setText(findTestObject('Login/android.widget.TextView - Hola'), 'Hola', Hola)
 
